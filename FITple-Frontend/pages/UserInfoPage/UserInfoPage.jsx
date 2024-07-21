0f01a6950260511689b6f2906715b6a1a2bdbf4f
SeyeonJang <seye0n.bliss@gmail.com>
2024-07-21 23:32:20 +0900

    feat: [#11] UserInfoPage style 별도 파일로 분리

--- FITple-Frontend/pages/UserInfoPage/UserInfoPage.jsx
+++ FITple-Frontend/pages/UserInfoPage/UserInfoPage.jsx
@@ -1,6 +1,27 @@
-import styled from 'styled-components'
 import profile from '../../assets/profile.svg'
 import { useState } from 'react'
+import {
+    UserInfoPageWrapper,
+    MainText,
+    SubText,
+    Container,
+    InfoContainer,
+    InfoWrapper,
+    TextWrapper,
+    InputWrapper,
+    InputBox,
+    GenderWrapper,
+    GenderButton,
+    FitWrapper,
+    FitButton,
+    StyleWrapper,
+    StyleButton,
+    BodyInfoContainer,
+    BodyInputWrapper,
+    BodyInputBox,
+    UnitText,
+    SubmitButton
+} from './UserInfoPage.style';
 
 function UserInfoPage() {
     const [selectedGender, setSelectedGender] = useState(null);
@@ -119,205 +140,5 @@ function UserInfoPage() {
         </UserInfoPageWrapper>
     );
 }
-
-const UserInfoPageWrapper = styled.div`
-    width: 100vw;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: start;
-    margin: 30px 0;
-    padding-bottom: 40px;
-`;
-
-const MainText = styled.p`
-    font-size: 20px;
-    font-weight: bold;
-`;
-
-const SubText = styled.p`
-    font-size: 17px;
-    margin-bottom: 30px;
-`;
-
-const Container = styled.div`
-    height: 135px;
-`;
-
-const InfoContainer = styled.div`
-    width: 70%;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    border: 1px solid black;
-    border-radius: 20px;
-    margin-top: 30px;
-    margin-bottom: 10px;
-    padding: 10px 40px;
-`;
-
-const InfoWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: start;
-    margin-top: 10px;
-`;
-
-const TextWrapper = styled.div`
-    width: 80px;
-    display: flex;
-    flex-direction: column;
-    align-items: end;
-`;
-
-const InputWrapper = styled.div`
-    width: 78%;
-    display: flex;
-    flex-direction: column;
-    padding-left: 20px;
-`;
-
-const InputBox = styled.input.attrs(props => ({
-        type: props.type || 'text', // 기본값을 'text'로 설정
-        id: props.id,
-        placeholder: props.placeholder // 힌트 텍스트를 props로 받음
-    }))`
-    width: 80%;
-    height: 30px;
-    margin-top: 10px;
-    margin-bottom: 20px;
-    border: 1px solid black;
-    border-radius: 10px;
-    background-color: white;
-    padding: 5px 15px;
-    font-size: 17px;
-    outline-color: #0075FF;
-`;
-
-const GenderWrapper = styled.div`
-    width: 84%;
-    display: flex;
-    justify-content: center;
-    gap: 10px;
-    margin-top: 10px;
-    margin-bottom: 20px;
-`;
-
-const GenderButton = styled.button`
-    width: 100%;
-    padding: 10px 20px;
-    border: ${({ isSelected }) => (isSelected ? 'none' : '1px solid black')};
-    border-radius: 10px;
-    background-color: ${({ isSelected }) => (isSelected ? '#0075FF' : 'white')};
-    color: ${({ isSelected }) => (isSelected ? 'white' : 'black')};
-    cursor: pointer;
-
-    &:focus {
-        outline: none;
-    }
-`;
-
-const FitWrapper = styled.div`
-    width: 84%;
-    display: flex;
-    justify-content: center;
-    gap: 10px;
-    margin-top: 10px;
-    margin-bottom: 20px;
-`;
-
-const FitButton = styled.button`
-    width: 100%;
-    padding: 10px 20px;
-    border: ${({ isSelected }) => (isSelected ? 'none' : '1px solid black')};
-    border-radius: 25px;
-    background-color: ${({ isSelected }) => (isSelected ? '#0075FF' : 'white')};
-    color: ${({ isSelected }) => (isSelected ? 'white' : 'black')};
-    cursor: pointer;
-
-    &:focus {
-        outline: none;
-    }
-`;
-
-const StyleWrapper = styled.div`
-    width: 90%;
-    display: flex;
-    justify-content: start;
-    gap: 10px;
-    margin-top: 10px;
-    margin-bottom: 20px;
-    flex-wrap: wrap;
-`;
-
-const StyleButton = styled.button`
-    width: 160px;
-    padding: 10px 20px;
-    border: ${({ isSelected }) => (isSelected ? 'none' : '1px solid black')};
-    border-radius: 25px;
-    background-color: ${({ isSelected }) => (isSelected ? '#0075FF' : 'white')};
-    color: ${({ isSelected }) => (isSelected ? 'white' : 'black')};
-    cursor: pointer;
-    margin: 5px;
-
-    &:focus {
-        outline: none;
-    }
-`;
-
-const BodyInfoContainer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: start;
-    justify-content: space-around;
-`;
-
-const BodyInputWrapper = styled.div`
-    position: relative;
-    width: 90%;
-    margin-top: 10px;
-    margin-bottom: 23px;
-`;
-
-const BodyInputBox = styled.input.attrs(props => ({
-        type: props.type || 'text', // 기본값을 'text'로 설정
-        id: props.id,
-        placeholder: props.placeholder // 힌트 텍스트를 props로 받음
-    }))`
-    width: 100%;
-    height: 40px;
-    border: 1px solid black;
-    border-radius: 10px;
-    background-color: white;
-    padding: 5px 40px 5px 15px;
-    font-size: 17px;
-    outline-color: #0075FF;
-    text-align: right;
-    box-sizing: border-box;
-`;
-
-// 단위 텍스트를 스타일링하는 컴포넌트
-const UnitText = styled.span`
-    position: absolute;
-    right: 15px;
-    top: 50%;
-    transform: translateY(-50%);
-    font-size: 1
-    pointer-events: none;
-`;
-
-const SubmitButton = styled.button`
-    width: 150px;
-    height: 50px;
-    background-color: black;
-    border-radius: 10px;
-    font-size: 17px;
-    color: white;
-    margin-top: 25px;
-`;
 
 export default UserInfoPage
--- FITple-Frontend/src/App.jsx
+++ FITple-Frontend/src/App.jsx
@@ -1,61 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styled from 'styled-components'
-import UserInfoPage from "../pages/UserInfoPage/UserInfoPage";
-import LoginPage from "../pages/LoginPage/LoginPage";
-import SignupPage from "../pages/SignupPage/SignupPage";
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <AppWrapper>
-        <Routes>
-          <Route path='/' element={<SignupPage/>}/>
-        </Routes>
-      </AppWrapper>
-    </BrowserRouter>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
-const AppWrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
-
 export default App
-
-
-// import { useState } from 'react'
-// import reactLogo from '../assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
