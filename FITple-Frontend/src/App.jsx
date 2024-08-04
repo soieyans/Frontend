34bf774dadaad3209f5f18412a238fe82993d9e2
kangsuyeong <ekdh16@naver.com>
2024-08-04 09:16:47 +0900

    refactor: [#23] 로고이미지 경로 변경

--- FITple-Frontend/src/App.jsx
+++ FITple-Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Navbar />
       {/* 각자 route 설정하기 */}
       <Routes>
-        <Route />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
