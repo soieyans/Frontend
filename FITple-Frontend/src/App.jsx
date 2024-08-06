ca06ce40adadce9aa7564985f833b3c8b3193c50
kangsuyeong <ekdh16@naver.com>
2024-08-06 19:03:14 +0900

    Refactor: [#25] Navbar 라우터연결

--- FITple-Frontend/src/App.jsx
+++ FITple-Frontend/src/App.jsx
@@ -6,22 +6,25 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import ClothdetailPage from "./pages/ClothdetailPage/ClothdetailPage";
 import ClothmainPage from "./pages/ClothmainPage/ClothmainPage";
+import UserInfoPage from "./pages/UserInfoPage/UserInfoPage";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  console.log("Current path:", location.pathname);
+  // Navbar를 제외할 경로 목록
+  const excludeNavbarPaths = ["/", "/SignUp", "/user"];
   return (
     <>
-      {/* Navbar */}
-      <Navbar />
+      {/* 특정 경로에서만 Navbar 렌더링 */}
+      {!excludeNavbarPaths.includes(location.pathname) && <Navbar />}
       {/* 각자 route 설정하기 */}
       <Routes>
-        {/* 로그인페이지 */}
-        <Route path="/login" element={<LoginPage />} />
-        {/* 회원가입페이지 */}
+        <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        {/* 상품디테일 */}
-        <Route path="/product" element={<ClothdetailPage />} />
-        {/* 이게 메인페이지? */}
-        <Route path="/" element={<ClothmainPage />} />
+        <Route path="/user" element={<UserInfoPage />} />
+        <Route path="/cloth" element={<ClothmainPage />} />
+        <Route path="/detail" element={<ClothdetailPage />} />
       </Routes>
     </>
   );
