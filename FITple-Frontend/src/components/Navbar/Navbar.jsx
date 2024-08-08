f2e11ad2d227f7ee4a5f1b91891b4cf5541189eb
kangsuyeong <ekdh16@naver.com>
2024-08-08 14:52:15 +0900

    feat: [#33] Navbar 검색 페이지 blue ver 구현

--- FITple-Frontend/src/components/Navbar/Navbar.jsx
+++ FITple-Frontend/src/components/Navbar/Navbar.jsx
@@ -14,9 +14,9 @@ import {
   LogoTitle,
 } from "./Navbar.style";
 
-const Navbar = () => {
+const Navbar = ({ blue }) => {
   return (
-    <Container>
+    <Container blue={blue}>
       {/* 전체 Navbar 가운데 정렬 */}
       <NavBarBox>
         {/* 로고 & 메뉴 영역 */}
@@ -24,18 +24,24 @@ const Navbar = () => {
           {/* 로고 */}
           <LogoBox to="/cloth">
             <img src={Logo} />
-            <LogoTitle>FITple</LogoTitle>
+            <LogoTitle blue={blue}>FITple</LogoTitle>
           </LogoBox>
           {/* 메뉴 */}
           <MenuBox>
-            <MenuItem to="/cloth">옷장</MenuItem>
-            <MenuItem to="/recommend">추천</MenuItem>
-            <MenuItem>검색</MenuItem>
-            <MenuItem to="/profile">프로필</MenuItem>
+            <MenuItem to="/cloth" blue={blue}>
+              옷장
+            </MenuItem>
+            <MenuItem to="/recommend" blue={blue}>
+              추천
+            </MenuItem>
+            <MenuItem blue={blue}>검색</MenuItem>
+            <MenuItem to="/profile" blue={blue}>
+              프로필
+            </MenuItem>
           </MenuBox>
         </MainBox>
         {/* 오른쪽 검색 영역 */}
-        <SearchBox>
+        <SearchBox blue={blue}>
           <SearchIconBox>
             <img src={SearchIcon} />
           </SearchIconBox>
--- FITple-Frontend/src/components/Navbar/Navbar.style.js
+++ FITple-Frontend/src/components/Navbar/Navbar.style.js
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 export const Container = styled.div`
+  background-color: ${(props) => (props.blue ? "#0075FF" : "white")};
   display: flex;
   justify-content: center;
-  margin-top: 20px;
+  padding-top: 20px;
 `;
 
 export const NavBarBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 87%;
+  width: 1253px;
 `;
 
 export const MainBox = styled.div`
   display: flex;
-  justify-content: space-between;
-  width: 57%;
+  width: 715px;
 `;
 export const LogoBox = styled(Link)`
   color: black;
@@ -25,9 +25,11 @@ export const LogoBox = styled(Link)`
   font-size: 28px;
   font-weight: 900;
   text-decoration: none;
+  margin-right: 84px;
 `;
 export const LogoTitle = styled.p`
   margin-left: 0.8em;
+  color: ${(props) => (props.blue ? "white" : "black")};
 `;
 export const LogoItem = styled.div`
   cursor: pointer;
@@ -42,18 +44,19 @@ export const MenuBox = styled.div`
 `;
 
 export const MenuItem = styled(Link)`
+  color: black;
   cursor: pointer;
   text-decoration: none;
-  color: black;
   &:hover {
-    color: #0276fe;
+    transform: scale(1.2);
+    color: ${(props) => (props.blue ? "white" : "#0276fe")};
     font-weight: 800;
   }
 `;
 export const SearchBox = styled.div`
   height: 61px;
   width: 31%;
-  display: flex;
+  display: ${(props) => (props.blue ? "none" : "flex")};
   align-items: center;
   position: relative;
 `;
@@ -80,4 +83,3 @@ export const SearchIconBox = styled.button`
   padding: 0px;
   left: 28px;
 `;
-
